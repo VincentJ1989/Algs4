1ada3a88d246444b9ee89e4400c0e451a478ca76
VincentJ <im.vincentj@gmail.com>
2018-11-21 22:31:55 +0800

    [feature]提交顺序查找(基于无序链表)和二分查找(基于有序数组)的代码。

---
+++ src/search/BinarySearchST.java
@@ -0,0 +1,108 @@
+package search;
+
+import edu.princeton.cs.algs4.Queue;
+
+/**
+ * 二分查找(基于有序数组)<br></br>
+ * 最优的查找效率和空间需求，能够进行有序性相关的操作，但插入操作很慢
+ */
+public class BinarySearchST<Key extends Comparable<Key>, Value> {
+    private Key[] keys;
+    private Value[] vals;
+    private int N;
+
+    public BinarySearchST(int capacity) {
+        keys = (Key[]) new Comparable[capacity];
+        vals = (Value[]) new Object[capacity];
+    }
+
+    public int size() {
+        return N;
+    }
+
+    public Value get(Key key) {
+        if (isEmpty()) {
+            return null;
+        }
+        int i = rank(key);
+        if (i < N && keys[i].compareTo(key) == 0) {
+            return vals[i];
+        } else {
+            return null;
+        }
+    }
+
+    public int rank(Key key){
+        int lo=0;
+        int hi=N-1;
+        while (lo<=hi){
+            int mid=lo+(hi-lo)/2;
+            int cmp=key.compareTo(keys[mid]);
+            if(cmp<0){
+                hi=mid-1;
+            }else if(cmp>0){
+                lo=mid+1;
+            }else {
+                return mid;
+            }
+        }
+        return lo;
+    }
+
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    public void put(Key key, Value val) {
+        // 查找键，找到则更新，否则创建新的元素
+        int i = rank(key);
+        if (i < N && keys[i].compareTo(key) == 0) {
+            vals[i] = val;
+            return;
+        }
+        for (int j = N; j > i; j--) {
+            keys[j] = keys[j - 1];
+            vals[j] = vals[j - 1];
+        }
+        keys[i] = key;
+        vals[i] = val;
+        N++;
+    }
+
+    // 简化代码未增加相关的判空
+    public Key min() {
+        return keys[0];
+    }
+
+    public Key max() {
+        return keys[N - 1];
+    }
+
+    public Key select(int k) {
+        return keys[k];
+    }
+
+    public Key ceiling(Key key) {
+        int i = rank(key);
+        return keys[i];
+    }
+
+    public Iterable<Key> keys(Key lo, Key hi) {
+        Queue<Key> q = new Queue<Key>();
+        for (int i = rank(lo); i < rank(hi); i++) {
+            q.enqueue(keys[i]);
+        }
+        if (contains(hi)) {
+            q.enqueue(keys[rank(hi)]);
+        }
+        return q;
+    }
+
+    private boolean contains(Key key) {
+        return get(key) != null;
+    }
+
+    public void delete(Key key) {
+        put(key, null);
+    }
+}
---
+++ src/search/SequentialSearchST.java
@@ -0,0 +1,49 @@
+package search;
+
+/**
+ * 顺序查找(基于无序链表)<br></br>
+ * 适用于小型的问题
+ */
+public class SequentialSearchST<Key, Value> {
+    // 链表首节点
+    private Node first;
+
+    private class Node {
+        // 链表节点的定义
+        Key key;
+        Value val;
+        Node next;
+
+        public Node(Key key, Value val, Node next) {
+            this.key = key;
+            this.val = val;
+            this.next = next;
+        }
+
+        public Value get(Key key) {
+            // 查找给定的键，返回相关联的值
+            for (Node x = first; x != null; x = x.next) {
+                if (key.equals(x.key)) {
+                    // 命中
+                    return x.val;
+                }
+            }
+            // 未命中
+            return null;
+        }
+
+        public void put(Key key, Value val) {
+            // 查找给定的值，找到则更新其值，邹泽在表中新建节点
+            for (Node x = first; x != null; x = x.next) {
+                if (key.equals(x.key)) {
+                    // 命中，更新
+                    x.val = val;
+                    return;
+                }
+            }
+            // 未命中，新建节点
+            first = new Node(key, val, first);
+        }
+
+    }
+}
